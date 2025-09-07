12b86d194d377ef28f89e2de1605f967f4eb42ee
Tranduy1dol <tmd.id2004@gmail.com>
2025-09-07 06:29:48 +0000

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,13 @@
 import type { NextConfig } from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  /**
-   * Set the base path to '/<repo-name>' for GitHub Pages.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/next-config-js/basePath
-   */
-  basePath: isProd ? `/${repoName}` : '',
+  output: 'export',
 
-  /**
-   * Set the asset prefix to '/<repo-name>' for GitHub Pages.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/next-config-js/assetPrefix
-   */
-  assetPrefix: isProd ? `/${repoName}` : '',
+  // These should be empty for a `username.github.io` repository
+  basePath: '',
+  assetPrefix: '',
 
-  // React's Strict Mode is a good practice.
   reactStrictMode: true,
-
-  /**
-   * Disable server-based image optimization. Next.js' default image optimization
-   * is not compatible with static exports.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-   */
   images: {
     unoptimized: true,
   },
